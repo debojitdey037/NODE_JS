d8a061bc9bce633000631544c47c47631d48b922
Debojit Dey. <debojitdey.@Debojits-MacBook-Air.local>
2026-04-11 10:45:19 +0530

    add

---
+++ Lec02/script.js
@@ -0,0 +1,28 @@
+const express = require("express")    //import express from node_modules
+const app = express()
+const PORT=4000;
+
+// query params
+app.get("/search",(req,res)=>{
+  const query = req.query;
+  console.log(query);
+  res.send("query received",query)
+})
+
+app.get("/user/:id/data",(req,res)=>{
+  const params = req.params
+  console.log(params);
+  res.send("params recieved",params)
+  
+})
+app.use(express.json())
+app.post("/user/create",(req,res)=>{
+  const body = req.body;
+  console.log(body);
+  res.send("body recieved",body)
+})
+
+// start server , port -> mapping of process in system
+app.listen(PORT,()=>{
+  console.log("server is started at" ,`http://localhost:${PORT}`);
+})
---
+++ Lec03/script1.js
@@ -0,0 +1,49 @@
+//middleware
+const express = require("express")
+const app = express()
+const PORT = 4000
+
+//inbuilt middleware
+app.use(express.json())
+
+//custom middleware
+app.use((req,res,next)=>{
+    console.log(req.path);
+    console.log("custom middleware");
+    next()
+    // its works to pass the req to next middleware
+})
+
+app.get("/",(req,res)=>{
+    res.send("hello from server")
+})
+//path specific middleware
+//this will run for the API's which starts with "/user"
+app.use("/user",(req,res,next)=>{
+    console.log("user middleware");
+    next()
+})
+
+//this will run for the API's which starts with "/product"
+app.use("/product",(req,res,next)=>{
+    console.log("product middleware");
+    next()
+    // its works to pass the req to next middleware
+})
+
+
+app.get("/user/:id/data",(req,res)=>{
+    const params = req.params
+    const id = params.id
+    console.log("user",id);
+    res.send(id)
+})
+
+app.get("/product/:id/data",(req,res)=>{
+    const params = req.params
+    const id = params.id
+    console.log("product",id);
+    res.send(id)
+})
+
+app.listen(PORT,() => console.log("server is running on port " + PORT))
