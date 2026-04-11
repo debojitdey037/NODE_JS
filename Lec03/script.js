ee415723dfd83f249788c3c5559d0e4a42c4fc60
Debojit Dey. <debojitdey.@Debojits-MacBook-Air.local>
2026-04-11 19:28:04 +0530

    add

--- Lec03/script.js
+++ Lec03/script.js
@@ -9,7 +9,7 @@ app.use(express.json())
 //custom middleware
 app.use((req,res,next)=>{
     console.log(req.path);
-    console.log("custom middleware");
+    console.log("custom middleware")
     next()
     // its works to pass the req to next middleware
 })
@@ -20,13 +20,13 @@ app.get("/",(req,res)=>{
 //path specific middleware
 //this will run for the API's which starts with "/user"
 app.use("/user",(req,res,next)=>{
-    console.log("user middleware");
+    console.log("user middleware")
     next()
 })
 
 //this will run for the API's which starts with "/product"
 app.use("/product",(req,res,next)=>{
-    console.log("product middleware");
+    console.log("product middleware")
     next()
     // its works to pass the req to next middleware
 })
@@ -35,15 +35,27 @@ app.use("/product",(req,res,next)=>{
 app.get("/user/:id/data",(req,res)=>{
     const params = req.params
     const id = params.id
-    console.log("user",id);
+    console.log("user",id)
     res.send(id)
 })
 
 app.get("/product/:id/data",(req,res)=>{
     const params = req.params
     const id = params.id
-    console.log("product",id);
+    console.log("product",id)
     res.send(id)
+})
+
+const routerLevel = (req,res,next)=>{
+    console.log("router level")
+    next()
+    
+}
+
+app.get("/info",routerLevel,(req,res)=>{
+    console.log("info api")
+    res.send("report generated")
+    
 })
 
 app.listen(PORT,() => console.log("server is running on port " + PORT))
